31ff50fdd231e5b8670ef699d01cf2a4a26882c2
abar2000 <139106616+abar2000@users.noreply.github.com>
2024-11-21 08:59:33 +0530

    commit

--- src/app/checkbox/page.tsx
+++ src/app/checkbox/page.tsx
@@ -13,7 +13,22 @@ export default function Page ()  {
       <Checkbox id={'1'} checked={isChecked} onChange={setIsChecked} Skin={toggle}/> <br/> <br/> 
       <Checkbox id={'2'} checked={isChecked1} onChange={setIsChecked1} Skin={switcher}/> <br/>
       <Checkbox id={'3'} checked={isChecked2} onChange={setIsChecked2} Skin={wallswitch}/> <br/>
+
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="relative inline-flex w-24 h-12 bg-gray-300 rounded-full shadow-inner">
+     
+      <span
+        className="absolute left-0 w-12 h-12 bg-white rounded-full shadow-md transition-transform transform"
+        id="toggleSlider"
+      ></span>
+    
+      <div className="absolute inset-0 flex justify-between items-center px-3 text-gray-700 text-sm font-medium">
+        <span>ON</span>
+        <span>OFF</span>
+      </div>
     </div>
+  </div>
+      </div>
   )
 }
 
