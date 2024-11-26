d99184ea40c4e59425b480e3e6eaf7cbc8cb1b00
Rajinthan17 <rajinthan.rk@gmail.com>
2024-11-26 15:57:40 +0530

    Fix

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    poweredByHeader: false,
+    generateEtags: false,
+    reactStrictMode: false,
+    output: "standalone",
+    trailingSlash: false,
+    env: {
+  },
+}
+
+export default nextConfig
